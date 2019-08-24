c16ecfe8035a40866fb7798008a2fdd78798515e
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-08-24 17:32:25 +0530

    add detail page for pet

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
+import pet from '@frontendmasters/pet'
 
-const Details = props => {
-  return (
-    <div>
-      <pre>
-        <code>{JSON.stringify(props, null, 4)}</code>
-      </pre>
-    </div>
-  )
+class Details extends Component {
+  state = {
+    loading: true
+  }
+  componentDidMount() {
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) =>
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.phots,
+          breed: animal.breeds.primary,
+          loading: false
+        })
+      )
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  render() {
+    if (this.state.loading) {
+      return <p>Loading...</p>
+    }
+    const { name, animal, location, description, media, breed } = this.state
+    return (
+      <div className='details'>
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  }
 }
 export default Details
