b6e75491683c50e1355c04d3c1ffcdbaa803ccb9
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-08-24 17:56:28 +0530

    add carousel for pet photos

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import pet from '@frontendmasters/pet'
+import Carousel from './Carousel'
 
 class Details extends Component {
   state = {
@@ -14,7 +15,7 @@ class Details extends Component {
           animal: animal.type,
           location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
           description: animal.description,
-          media: animal.phots,
+          media: animal.photos,
           breed: animal.breeds.primary,
           loading: false
         })
@@ -30,6 +31,7 @@ class Details extends Component {
     const { name, animal, location, description, media, breed } = this.state
     return (
       <div className='details'>
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
