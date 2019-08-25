a530435c024eb15c80cc882c7a5b86cc63c7497d
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-08-25 09:20:26 +0530

    Revert "add emotion and setup for babel"
    This reverts commit eb94b1ce2aece34605174f3a62842dede9763783.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@reach/router'
+import { Router, Link } from '@reach/router'
 import SearchParams from './SearchParams'
 import Details from './Details'
 import ThemeContext from './ThemeContext'
-import NavBar from './NavBar'
 const App = () => {
   const themeHook = useState('darkblue')
   return (
     <ThemeContext.Provider value={themeHook}>
       <div>
-        <NavBar />
+        <header>
+          <Link to='/'>Adopt me</Link>
+        </header>
         <Router>
           <SearchParams path='/' />
           <Details path='/details/:id' />
