008172219af6223f17ed731daf4e685fcdaaca4b
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-05 00:44:19 +0530

    add redux and create actions and reducers

---
+++ src/reducers/location.js
@@ -0,0 +1,6 @@
+export default function location(state = 'Seattle WA', action) {
+  if (action.type === 'CHANGE_LOCATION') return action.payload
+  else {
+    return state
+  }
+}
---
+++ src/reducers/theme.js
@@ -0,0 +1,6 @@
+export default function theme(state = 'darkblue', action) {
+  if (action.type === 'CHANGE_THEME') return action.payload
+  else {
+    return state
+  }
+}
