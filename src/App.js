f2e00d285dc51dc0a1879fbeebe0c11ca9041f02
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-05 00:45:23 +0530

    remove context from app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Link } from '@reach/router'
 import SearchParams from './SearchParams'
 import Details from './Details'
-import ThemeContext from './ThemeContext'
+
 const App = () => {
-  const themeHook = useState('darkblue')
   return (
-    <ThemeContext.Provider value={themeHook}>
-      <div>
-        <header>
-          <Link to='/'>Adopt me</Link>
-        </header>
-        <Router>
-          <SearchParams path='/' />
-          <Details path='/details/:id' />
-        </Router>
-      </div>
-    </ThemeContext.Provider>
+    <div>
+      <header>
+        <Link to='/'>Adopt me</Link>
+      </header>
+      <Router>
+        <SearchParams path='/' />
+        <Details path='/details/:id' />
+      </Router>
+    </div>
   )
 }
 ReactDOM.render(<App />, document.getElementById('root'))
